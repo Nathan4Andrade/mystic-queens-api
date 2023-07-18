405d805aac739366b2ce43f18aa7923b1604eabd
Nathan4Andrade <nathanandrade20@gmail.com>
2023-07-18 10:59:42 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log("hello world!");
+import express from "express";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
